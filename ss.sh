59194011c6f99ff93a430d66af3420e2fd897a29
Caligula <snax@snerx.com>
2017-06-28 22:08:03 -0700

    screen script updated

--- ss.sh
+++ ss.sh
@@ -3,9 +3,9 @@
 
 if [ "$1" == "window" ]
 then
-	scrot 'scrot-17%m%d_%M-%s.png' -s -e 'mv $f ~/Pictures/scrots/'
+	scrot 'scrot17%m%d_%M-%s.png' -s -e 'mv $f ~/Pictures/scrots/'
 else
-	scrot 'scrot-17%m%d_%M-%s.png' -e 'mv $f ~/Pictures/scrots/'
+	scrot 'scrot17%m%d_%M-%s.png' -e 'mv $f ~/Pictures/scrots/'
 fi
 
 printf "\n  Screenshot taken.\n\n"
