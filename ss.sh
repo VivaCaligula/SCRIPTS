17d06277ba2a49d68dcee1eee926e09637578b79
Caligula <snax@snerx.com>
2017-07-07 01:20:17 -0700

    screenshot fix

--- ss.sh
+++ ss.sh
@@ -8,6 +8,6 @@ else
 	scrot 'scrot17%m%d_%M-%s.png' -e 'mv $f ~/Pictures/scrots/'
 fi
 
-dunst -p "Screenshot taken."
+#dunst -p "Screenshot taken."
 
-#printf "\n  Screenshot taken.\n\n"
+printf "\n  Screenshot taken.\n\n"
