f3cab45123b67fe8c353ed20fb6369080cdced61
Caligula <snax@snerx.com>
2017-07-06 19:11:43 -0700

    lockscreen script added

---
+++ lock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Params can be found here- https://github.com/chrjguill/i3lock-color
+
+i3lock -k -B=8 --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c
