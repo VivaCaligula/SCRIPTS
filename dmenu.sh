a3621089059e48e6b7318ba6b51c2c51dfd4b64e
Caligula <snax@snerx.com>
2017-07-14 15:56:14 -0700

    wal fix

--- dmenu.sh
+++ dmenu.sh
@@ -4,6 +4,6 @@
 #==================================#
 
 # Set the custom colors from wal.
-source "$HOME/.cache/wal/colors.sh"`
+source "$HOME/.cache/wal/colors.sh"
 
 dmenu_run -nb "$color0" -nf "$color7" -sb "$color1" -sf "$color7" -fn Terminus-15:normal
