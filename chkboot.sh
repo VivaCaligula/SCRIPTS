7d65330e06f2f8b92bcbc678d0d66af5fc1ed83a
Caligula <snax@snerx.com>
2017-07-31 07:02:20 +0000

    Delete chkboot.sh

--- chkboot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#================================#
-#     Snax's chkboot script.     #
-#================================#
-
-sudo echo "Checking boot integrity..."
-sudo chkboot
-printf "\nBoot check complete.\n"
