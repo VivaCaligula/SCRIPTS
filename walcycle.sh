09f4e9c915809769cc1381bc9811b13e0910eb25
VivaCaligula <snax@snerx.com>
2017-08-18 01:48:00 -0700

    update for bspwm

--- walcycle.sh
+++ walcycle.sh
@@ -12,10 +12,7 @@ polybar rome &
 
 # Refresh bspwm's colorscheme.
 source "$HOME/.cache/wal/colors.sh"
-#color0=`sed -n '1p' ~/.cache/wal/colors`
-#olor1=`sed -n '2p' ~/.cache/wal/colors`
-#color7=`sed -n '8p' ~/.cache/wal/colors`
 bspc config normal_border_color $color0
-bspc config focused_border_color $color7
+bspc config focused_border_color $color1
 bspc config active_border_color $color1
-bspc config presel_feedback_color $color7
+bspc config presel_feedback_color $color1
