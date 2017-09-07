db153b8af2ceea909a2c4b53baac802071566c01
VivaCaligula <snax@snerx.com>
2017-09-07 03:12:43 -0700

    trying to get i3lock to work with wal

--- lock.sh
+++ lock.sh
@@ -5,4 +5,9 @@
 
 # Params can be found here- https://github.com/chrjguill/i3lock-color
 
+# Set the custom colors from wal.
+source "$HOME/.cache/wal/colors.sh"
+
+# i3lock -k -B=8 --textcolor=$color7 --insidecolor=$color1 --ringcolor=$color4 --linecolor=$color0 --keyhlcolor=$color0 --ringvercolor=$color0 --insidevercolor=$color0 --ringwrongcolor=$color4 --insidewrongcolor=$color8
+
 i3lock -k -B=8 --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c
