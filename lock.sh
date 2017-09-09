8810363d31e6a4c06ea75e96806f7edc5d69e725
VivaCaligula <snax@snerx.com>
2017-09-09 05:46:50 -0700

    color fix to work with wal

--- lock.sh
+++ lock.sh
@@ -10,6 +10,6 @@ source "$HOME/.cache/wal/colors.sh"
 
 # i3lock -k -B=8 --textcolor=$color7 --insidecolor=$color1 --ringcolor=$color4 --linecolor=$color0 --keyhlcolor=$color0 --ringvercolor=$color0 --insidevercolor=$color0 --ringwrongcolor=$color4 --insidewrongcolor=$color8
 
-i3lock -k -e -f -B=8 --timecolor="$color0"FF --timefont=terminus --timesize=32 --datecolor="$color0"FF --datestr="%Y/%m/%d" --datefont=terminus --datesize=24 --textcolor="$color7"80 --insidecolor="$color8"33 --ringcolor="$color1"3e --linecolor="$color1"80 --keyhlcolor="$color7"80 --bshlcolor="$color3"80 --separatorcolor="$color4"80 --veriftext="DRINKING VERIFICATION CAN" --ringvercolor="$color4"00 --insidevercolor="$color4"1c --wrongtext="WRONG" --ringwrongcolor="$color4"55 --insidewrongcolor="$color4"1c --textsize=12 --modsize=16 --radius=100
+i3lock -k -e -f -B=8 --timecolor="$color0"FF --timefont=terminus --timesize=32 --datecolor="$color0"FF --datestr="%Y/%m/%d" --datefont=terminus --datesize=24 --textcolor="$color7"80 --insidecolor="$color1"33 --ringcolor="$color1"3e --linecolor="$color1"80 --keyhlcolor="$color7"80 --bshlcolor="$color3"80 --separatorcolor="$color4"80 --veriftext="DRINKING VERIFICATION CAN" --ringvercolor="$color4"00 --insidevercolor="$color4"1c --wrongtext="WRONG" --ringwrongcolor="$color4"55 --insidewrongcolor="$color4"1c --textsize=12 --modsize=16 --radius=100
 
 dunstify --urgency=low "Screen locked!"
