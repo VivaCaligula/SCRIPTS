ec8b669ea22ab698aaad4548478a63a5beb0fcdb
VivaCaligula <snax@snerx.com>
2017-09-09 03:50:09 -0700

    screenshot script updated

--- ss.sh
+++ ss.sh
@@ -6,6 +6,7 @@
 # This was modified from a script by Andre Linusus - https://i.imgur.com/QqUCFik.png
 
 mkdir -p ~/Pictures/scrots/
+ICON="/usr/share/icons/Adwaita/16x16/devices/camera-photo.png"
 
 if [ "$1" == "window" ]
 then
@@ -15,5 +16,5 @@ else
 	scrot 'scrot17-%m-%d_%H-%M-%S.png' -e 'mv $f ~/Pictures/scrots/'
 fi
 
-#dunst -p "Screenshot taken."
+#dunst -p -i $ICON "Screenshot taken."
 printf "\n  Screenshot taken.\n\n"
