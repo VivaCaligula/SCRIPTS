9411df5a5b925cbbae12d206599a6cb6411f9abc
VivaCaligula <snax@snerx.com>
2017-09-20 03:05:16 -0700

    ANTIUM UPDATE!

--- dmenu.sh
+++ dmenu.sh
@@ -10,4 +10,4 @@ source "$HOME/scripts/scheme-CanvasedPastel.sh"
 # dmenu_run -nb "$color0" -nf "$color7" -sb "$color1" -sf "$color7" -fn Terminus-15:normal
 
 # For patched Dmenu2.
-dmenu_run -i -x 0 -y 0 -w 0 -nb "$color0" -nf "$foreground" -sb "$color1" -sf "$color7" -fn Terminus-16:normal
+dmenu_run -i -x 0 -y 0 -w 0 -nb "$color0" -nf "$color4" -sb "$color1" -sf "$foreground" -fn Terminus-16:normal
