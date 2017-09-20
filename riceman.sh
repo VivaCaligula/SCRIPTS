b12b6144d1f560460070aa7d56e899fcf85a6eea
VivaCaligula <snax@snerx.com>
2017-09-19 18:45:40 -0700

    ANTIUM UPDATE!

--- riceman.sh
+++ riceman.sh
@@ -5,12 +5,6 @@
 # This is a script for managing the different rice configurations
 # featured here - https://github.com/VivaCaligula/DOTFILES
 
-# Make sure script is run on linux.
-if [ $OSTYPE != "linux-gnu" ]; then
-    echo "Error: this script is only for Linux!"
-    exit 1
-fi
-
 echo "It's recommended you save and close any open nodes before switching."
 printf "\n"
 
@@ -18,23 +12,23 @@ while true
 do
     read -p "Switch aesthetic to ROME or ANTIUM? [r/a/q]: " answer
     case $answer in
-    [rR]* ) sudo cp -f ~/.config/bspwm/bwpwmRome ~/.config/bspwm/bspwmrc
+    [rR]* ) cp -f ~/.config/bspwm/bwpwmRome ~/.config/bspwm/bspwmrc
             echo "Switched bspwm config..."
-            sudo cp -f ~/.config/polybar/configRome ~/.config/polybar/config
+            cp -f ~/.config/polybar/configRome ~/.config/polybar/config
             echo "Switched polybar config..."
-            sudo cp -f ~/.config/termite/configRome ~/.config/termite/config
+            cp -f ~/.config/termite/configRome ~/.config/termite/config
             echo "Switched termite config..."
-            echo "Finished, killing of X session..."
+            echo "Finished, killing X session..."
             sleep 1
             pkill x
             break;;
-    [aA]* ) sudo cp -f ~/.config/bspwm/bwpwmAntium ~/.config/bspwm/bspwmrc
+    [aA]* ) cp -f ~/.config/bspwm/bwpwmAntium ~/.config/bspwm/bspwmrc
             echo "Switched bspwm config..."
-            sudo cp -f ~/.config/polybar/configAntium ~/.config/polybar/config
+            cp -f ~/.config/polybar/configAntium ~/.config/polybar/config
             echo "Switched polybar config..."
-            sudo cp -f ~/.config/termite/configAntium ~/.config/termite/config
+            cp -f ~/.config/termite/configAntium ~/.config/termite/config
             echo "Switched termite config..."
-            echo "Finished, killing of X session..."
+            echo "Finished, killing X session..."
             sleep 1
             pkill x
             break;;
