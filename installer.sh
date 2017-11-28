4eb36a2940a299ca3fab1f0fa873164054779b60
VivaCaligula <snax@snerx.com>
2017-11-27 23:15:25 -0800

    package updates

--- installer.sh
+++ installer.sh
@@ -16,12 +16,12 @@ sudo pacman-key --refresh-keys
 
 # Automatically dl + install all the programs you want from the arch repo; this will take a while.
 # Replace gdm with whatever your prefered DM is (as well as other prefered programs).
-sudo pacman --color always --force --noconfirm -S git zip ntp openssh openvpn zsh rxvt-unicode python-pip ttf-dejavu poppler inkscape fontforge awesome-terminal-fonts gnome gnome-tweak-tool networkmanager networkmanager-openvpn bspwm sxhkd termite feh compton ranger tmux htop scrot nmap firefox chromium keepassxc atom vim powerline powerline-fonts terminus-font screenfetch vlc phonon-qt4-vlc electrum hexchat transmission-qt libreoffice-still obs-studio ffmpeg gimp bleachbit checksec extundelete mpd ncmpcpp cmatrix thunar cool-retro-term dunst redshift gimp wine steam nethack dwarffortress 
+sudo pacman --color always --force --noconfirm -S git zip ntp openssh openvpn zsh rxvt-unicode python-pip ttf-dejavu poppler inkscape fontforge awesome-terminal-fonts gnome gnome-tweak-tool networkmanager networkmanager-openvpn bspwm sxhkd termite feh compton ranger tmux htop scrot nmap firefox chromium keepassxc atom vim powerline powerline-fonts terminus-font screenfetch vlc phonon-qt4-vlc electrum hexchat transmission-qt libreoffice-still obs-studio ffmpeg gimp bleachbit checksec extundelete mpd ncmpcpp cmatrix thunar cool-retro-term dunst redshift gimp wine steam kodi nethack dwarffortress shellcheck
 # dmenu replaced with patched dmenu2 in AUR
 sudo systemctl enable gdm.service
 
 # Automatically DL + install all programs from the AUR; this will take a while.
-yaourt --color --force --noconfirm -S w3m imagemagick hsetroot neofetch gtop polybar-git dmenu2 dunstify wal-git i3lock-color-git grub-customizer chkboot nerd-fonts-complete pacmanity discord python-tempora python-portend python-cheroot mnemosyne brainworkshop keybase-bin pkgbrowser 
+yaourt --color --force --noconfirm -S w3m imagemagick hsetroot neofetch gtop polybar dmenu2 dunstify python-pywal i3lock-color-git grub-customizer chkboot nerd-fonts-complete pacmanity discord python-tempora python-portend python-cheroot mnemosyne brainworkshop keybase-bin pkgbrowser ghostwriter
 
 # Whatever Shell you get, replace zsh with your prefered shell.
 which zsh
@@ -29,30 +29,43 @@ chsh -s /usr/bin/zsh
 sudo chsh -s /usr/bin/zsh
 # To check if user has zsh shell active:
 tail /etc/passwd
-# Additionally, you should get - http://ohmyz.sh/
+# Additionally, you should get http://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # If you want root to have the same, you will have to repeat this after $ su
+# Plugins for oh-my-zsh:
+cd "${ZSH_CUSTOM1:-$ZSH/custom}/plugins" || return
+git clone https://github.com/djui/alias-tips.git
+git clone https://github.com/Valiev/almostontop.git
+git clone https://github.com/arzzen/calc.plugin.zsh.git
+git clone https://github.com/zdharma/fast-syntax-highlighting.git
+git clone https://github.com/supercrabtree/k.git
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install # make sure this is appended to .zshrc: [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+git clone https://github.com/changyuheng/zsh-interactive-cd.git
+# Source the .plugin.zsh files in your ~/.zshrc (done already in Caligula's dotfiles).
+# Extra Ruby script to add icons to ls - https://github.com/athityakumar/colorls
+gem install colorls
 
-#To use GTK-like theme with Qt edit ~/.config/Trolltech.conf and set
+# To use GTK-like theme with Qt edit ~/.config/Trolltech.conf and set
 #   [Qt]
 #   style=GTK+
 # Extra ranger initialization (to allow img2txt in the terminal).
 ranger --copy-config=scope
 
 # Bitmap fonts, DL these if pacman's terminus-font doesn't work.
-cd ~
+cd ~ || return
 git clone https://github.com/Tecate/bitmap-fonts.git
-cd ~/bitmap-fonts
+cd ~/bitmap-fonts || return
 sudo cp -avr bitmap/ /usr/share/fonts
 xset fp+ /usr/share/fonts/bitmap
 fc-cache -fv
 ln -s ~/.Xresources ~/.Xdefaults
 xrdb ~/.Xresources
-cd ~
+cd ~ || return
 sudo rm -rf bitmap-fonts
 
 # DL the dotfiles for all the programs (automatically into their right paths) from git repo.
-cd /home
+cd /home || return
 git clone https://github.com/VivaCaligula/DOTFILES.git
 sudo mv -vf /home/DOTFILES/* ~/
 sudo rm -rf /home/DOTFILES
@@ -61,17 +74,13 @@ cp -rf powerlineconfigs/* /usr/lib/python3.6/site-packages/powerline/config_file
 
 # Atom Packages
 # Themes: dark-flat-ui, outlander-syntax, northern-atom-syntax, northern-atom-ui
-# Packages: Teletype, Minimap, Pigments, minimap-pigments, color-picker, split-diff, minimap-split-diff, highlight-line, highlight-selected, atom-clock, activate-power-mode, tree-view-autoresize
+# Packages: Teletype, Minimap, Pigments, minimap-pigments, color-picker, split-diff, minimap-split-diff, highlight-line, highlight-selected, atom-clock, activate-power-mode, tree-view-autoresize, linter-shellcheck
 # Disable stuff in general settings but also Settings > Packages > Whitespace
 
 # DL all your scripts.
-cd ~
+cd ~ || return
 git clone https://github.com/VivaCaligula/SCRIPTS.git
 mv SCRIPTS scripts
-# Extra Ruby script to add icons to ls - https://github.com/athityakumar/colorls
-gem install colorls
-# Extra Python script to add imageboard scrapper - https://github.com/pboardman/4scanner
-pip3 install 4scanner
 
 # To download the torbrowser.
 # Download the tarball from - https://www.torproject.org/download/download-easy.html.en#linux
@@ -79,6 +88,9 @@ pip3 install 4scanner
 # $ tar -xf tor-whatever
 # To download portable WinSCP - https://winscp.net/eng/download.php
 # $ unzip WinSCP*Portable.zip
+# To download Glances - https://nicolargo.github.io/glances/
+curl -L https://bit.ly/glances | /bin/bash
+rm glances
 
 # So that you don't have to "$ sudo wifi-menu" every time.
 sudo systemctl enable NetworkManager.service
