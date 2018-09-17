467af00c39218a3df30a13d3f4a8d5288f20beb8
Caligula <snerx@protonmail.com>
2018-09-16 23:10:50 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -16,12 +16,12 @@ sudo yaourt -Syu
 
 # Automatically dl + install all the programs you want from the arch repo; this will take a while.
 # Replace gdm with whatever your prefered DM is (as well as other prefered programs).
-sudo pacman --color always --force --noconfirm -S git zip unrar ntp openssh openvpn zsh rxvt-unicode python-pip ttf-dejavu poppler inkscape fontforge awesome-terminal-fonts gnome gnome-tweak-tool networkmanager networkmanager-openvpn bspwm sxhkd termite termite-terminfo feh compton ranger tmux htop scrot nmap firefox keepassxc atom vim powerline-fonts terminus-font screenfetch vlc phonon-qt4-vlc electrum zcash transmission-qt libreoffice-still ffmpeg gimp bleachbit checksec extundelete mpd ncmpcpp cmatrix thunar cool-retro-term dunst redshift wine steam kodi nethack shellcheck
+sudo pacman --color always --overwrite --noconfirm -S git zip unrar ntp openssh openvpn ruby zsh rxvt-unicode python-pip ttf-dejavu poppler inkscape fontforge awesome-terminal-fonts gnome gnome-tweaks networkmanager networkmanager-openvpn bspwm sxhkd termite termite-terminfo feh compton ranger tmux htop glances scrot nmap firefox keepassxc atom vim powerline-fonts terminus-font screenfetch vlc phonon-qt5-vlc electrum transmission-qt libreoffice-still ffmpeg gimp bleachbit checksec extundelete mpd ncmpcpp cmatrix thunar cool-retro-term dunst redshift wine steam nethack shellcheck keybase-gui
 # dmenu replaced with patched dmenu2 in AUR
 sudo systemctl enable gdm.service
 
 # Automatically DL + install all programs from the AUR; this will take a while.
-yaourt --color --force --noconfirm -S w3m imagemagick hsetroot micro neofetch gtop polybar dmenu2 dunstify python-pywal i3lock-color-git grub-customizer chkboot nerd-fonts-complete pacmanity discord-canary python-tempora python-portend python-cheroot mnemosyne brainworkshop keybase-bin pkgbrowser
+yaourt --color --force --noconfirm -S w3m imagemagick hsetroot micro neofetch gtop polybar dmenu2 dunstify python-pywal i3lock-color-git grub-customizer chkboot nerd-fonts-complete pacmanity discord-canary python-tempora python-portend python-cheroot mnemosyne brainworkshop pkgbrowser tor-browser
 
 # Whatever Shell you get, replace zsh with your prefered shell.
 which zsh
@@ -29,7 +29,7 @@ chsh -s /usr/bin/zsh
 sudo chsh -s /usr/bin/zsh
 # To check if user has zsh shell active:
 tail /etc/passwd
-# Additionally, you should get http://ohmyz.sh/
+# Additionally, you should get https://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # If you want root to have the same, you will have to repeat this after $ su
 # Plugins for oh-my-zsh:
@@ -46,9 +46,6 @@ git clone https://github.com/changyuheng/zsh-interactive-cd.git
 # Extra Ruby script to add icons to ls - https://github.com/athityakumar/colorls
 gem install colorls
 
-# To use GTK-like theme with Qt edit ~/.config/Trolltech.conf and set
-#   [Qt]
-#   style=GTK+
 # Extra ranger initialization (to allow img2txt in the terminal).
 ranger --copy-config=scope
 
@@ -66,10 +63,9 @@ sudo rm -rf bitmap-fonts
 
 # DL the dotfiles for all the programs (automatically into their right paths) from git repo.
 cd /home || return
-git clone https://github.com/VivaCaligula/DOTFILES.git
-sudo mv -vf /home/DOTFILES/* ~/
+sudo git clone https://github.com/VivaCaligula/DOTFILES.git
+sudo cp -vfr /home/DOTFILES/* ~/
 sudo rm -rf /home/DOTFILES
-# $ polybar -r bar_name # Refreshes polybar's cache.
 
 # Atom Packages
 # Themes: dark-flat-ui, outlander-syntax, northern-atom-syntax, northern-atom-ui
@@ -81,21 +77,14 @@ cd ~ || return
 git clone https://github.com/VivaCaligula/SCRIPTS.git
 mv SCRIPTS scripts
 
-# To download the torbrowser.
-# Download the tarball from - https://www.torproject.org/download/download-easy.html.en#linux
-# $ cd ~/Downloads 
-# $ tar -xf tor-whatever
 # To download portable WinSCP - https://winscp.net/eng/download.php
 # $ unzip WinSCP*Portable.zip
-# To download Glances - https://nicolargo.github.io/glances/
-curl -L https://bit.ly/glances | /bin/bash
-rm glances
 
 # So that you don't have to "$ sudo wifi-menu" every time.
 sudo systemctl enable NetworkManager.service
 # To replace vi with nano as the default text editor for commands such as visudo, set the VISUAL and EDITOR environment variables:
-# export VISUAL=nano
-# export EDITOR=nano
+export VISUAL=nano
+export EDITOR=nano
 
 # Restart your computer, log in with your new DM into bspwm, and you're done.
 systemctl reboot
